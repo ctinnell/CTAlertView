68e8b85865d198eaadb75708c5a14cab11b18d51
Clay Tinnell <ctinnell@gaig.com>
2016-02-20 08:02:12 -0500

    refactor controller to new folder

---
+++ CTAlertView/Alert/CTAlertView.swift
@@ -0,0 +1,21 @@
+//
+//  CTAlertView.swift
+//  CTAlertView
+//
+//  Created by Clay Tinnell on 2/20/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class CTAlertView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
