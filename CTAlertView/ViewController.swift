5b3f7e700b04d8d5589ec38d38151589b467b67a
Clay Tinnell <ctinnell@gaig.com>
2016-02-23 13:53:23 -0500

    instantiate alert controller

--- CTAlertView/ViewController.swift
+++ CTAlertView/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        let alertView = CTAlertController(parentFrame: self.view.frame, type: .Banner, position: .Top)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
